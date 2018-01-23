a7765fe5acba7dcc51c4413bd108e6b1437a9954
Automatic Deployment (Travis CI) <none@none.none>
2018-01-23 04:08:22 +0000

    Documentation Update: 3f58d8655b4533024711b58940c684eb9309b646

---
+++ search/typedefs_3.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['databoxstring',['DataBoxString',['../group__DataBoxGroup.html#ga38df73b5a42be1c705df06f510409121',1,'db']]],
+  ['deriv_5flapse',['deriv_lapse',['../classEinsteinSolutions_1_1KerrSchild.html#aaa078d3a037fccf9075eeba80a17d386',1,'EinsteinSolutions::KerrSchild']]],
+  ['deriv_5fshift',['deriv_shift',['../classEinsteinSolutions_1_1KerrSchild.html#a89e07890da5a59231e6a43170d43966e',1,'EinsteinSolutions::KerrSchild']]],
+  ['deriv_5fspatial_5fmetric',['deriv_spatial_metric',['../classEinsteinSolutions_1_1KerrSchild.html#abd907a610653213942c43a03ec4aaa6c',1,'EinsteinSolutions::KerrSchild']]],
+  ['derivative_5fframe',['derivative_frame',['../ScalarWave_2Equations_8cpp.html#a73cb4f286a3b02a20c9ceb21d181262b',1,'Equations.cpp']]],
+  ['derivative_5ftags',['derivative_tags',['../ScalarWave_2Equations_8cpp.html#acb8235359c7d5c376bb9c044ba7792c4',1,'Equations.cpp']]],
+  ['difference_5ftype',['difference_type',['../classDataVector.html#acad716aab5c674ed5733937407bd2f20',1,'DataVector::difference_type()'],['../classVariables_3_01tmpl_1_1list_3_01Tags_8_8_8_01_4_01_4.html#a558f1ac85ff8c26ea5ecde9d8466c048',1,'Variables&lt; tmpl::list&lt; Tags... &gt; &gt;::difference_type()']]],
+  ['du_5fdt',['du_dt',['../structScalarWave_1_1System.html#ae43818011f399b193ab13cfbe162fe3e',1,'ScalarWave::System']]]
+];
---
+++ src/Domain/Tags.hpp
@@ -0,0 +1,117 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+/// \file
+/// Defines tags related to domain quantities
+
+#pragma once
+
+#include <memory>
+#include <unordered_map>
+
+#include "DataStructures/DataBoxTag.hpp"
+#include "DataStructures/Index.hpp"
+#include "Domain/CoordinateMaps/CoordinateMap.hpp"
+#include "Domain/Direction.hpp"
+#include "Domain/Element.hpp"
+#include "Domain/FaceNormal.hpp"
+#include "Domain/LogicalCoordinates.hpp"
+
+class DataVector;
+template <size_t Dim>
+class Element;
+template <size_t Dim>
+class Index;
+template <size_t Dim, typename TargetFrame>
+class ElementMap;
+namespace Frame {
+struct Logical;
+struct Inertial;
+}  // namespace Frame
+
+namespace Tags {
+/// \ingroup DataBoxTagsGroup
+/// \ingroup ComputationalDomainGroup
+/// The ::Element associated with the DataBox
+template <size_t VolumeDim>
+struct Element : db::DataBoxTag {
+  static constexpr db::DataBoxString label = "Element";
+  using type = ::Element<VolumeDim>;
+};
+
+/// \ingroup DataBoxTagsGroup
+/// \ingroup ComputationalDomainGroup
+/// The extents of DataVectors in the DataBox
+template <size_t VolumeDim>
+struct Extents : db::DataBoxTag {
+  static constexpr db::DataBoxString label = "Extents";
+  using type = ::Index<VolumeDim>;
+};
+
+/// \ingroup DataBoxTagsGroup
+/// \ingroup ComputationalDomainGroup
+/// The logical coordinates in the Element
+template <size_t VolumeDim>
+struct LogicalCoordinates : db::ComputeItemTag {
+  static constexpr db::DataBoxString label = "LogicalCoordinates";
+  using argument_tags = tmpl::list<Tags::Extents<VolumeDim>>;
+  static constexpr auto function = logical_coordinates<VolumeDim>;
+};
+
+/// \ingroup DataBoxTagsGroup
+/// \ingroup ComputationalDomainGroup
+/// The coordinate map from logical to grid coordinate
+template <size_t VolumeDim>
+struct ElementMap : db::DataBoxTag {
+  static constexpr db::DataBoxString label = "ElementMap";
+  using type = std::unique_ptr<::CoordinateMapBase<Frame::Logical, Frame::Grid,
+                                                   VolumeDim>>;
+};
+
+/// \ingroup DataBoxTagsGroup
+/// \ingroup ComputationalDomainGroup
+/// Computes the inverse Jacobian of the map held by `MapTag` at the coordinates
+/// held by `SourceCoordsTag`. The coordinates must be in the source frame of
+/// the map.
+template <typename MapTag, typename SourceCoordsTag>
+struct InverseJacobian : db::ComputeItemTag, db::DataBoxPrefix {
+  using tag = MapTag;
+  static constexpr db::DataBoxString label = "InverseJacobian";
+  static constexpr auto function(
+      const db::item_type<MapTag>& element_map,
+      const db::item_type<SourceCoordsTag>& source_coords) noexcept {
+    return element_map.inv_jacobian(source_coords);
+  }
+  using argument_tags = typelist<MapTag, SourceCoordsTag>;
+};
+
+
+namespace detail {
+template <size_t VolumeDim>
+auto make_unnormalized_face_normals(
+    const Index<VolumeDim>& extents,
+    const std::unique_ptr<CoordinateMapBase<Frame::Logical, Frame::Grid,
+                                            VolumeDim>>& map) noexcept {
+  std::unordered_map<Direction<VolumeDim>,
+                     tnsr::i<DataVector, VolumeDim, Frame::Grid>>
+      result;
+  for (const auto& d : Direction<VolumeDim>::all_directions()) {
+    result.emplace(d, unnormalized_face_normal(
+                          extents.slice_away(d.dimension()), *map, d));
+  }
+  return result;
+}
+}  // namespace detail
+
+/// \ingroup DataBoxTagsGroup
+/// \ingroup ComputationalDomainGroup
+/// The unnormalized grid normal one form on each side
+template <size_t VolumeDim>
+struct UnnormalizedFaceNormal : db::ComputeItemTag {
+  static constexpr db::DataBoxString label = "UnnormalizedFaceNormal";
+  static constexpr auto function =
+      detail::make_unnormalized_face_normals<VolumeDim>;
+  using argument_tags = tmpl::list<Extents<VolumeDim>, ElementMap<VolumeDim>>;
+};
+
+}  // namespace Tags
